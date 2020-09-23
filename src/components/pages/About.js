8a1f6d339e646b040cbeb7f53fa22ab675cb112f
Xavier Jackson <xavierejackson20@gmail.com>
2020-09-22 21:25:19 -0400

    added pages with react.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>This is where my brand statement will go!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>Portfolio page information coming soon!</p>
+    </div>
+  );
+}
+
+export default Portfolio;
