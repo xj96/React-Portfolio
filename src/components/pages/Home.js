eccf3d7a691e4204f1e19d8f39d8f518e5445767
Xavier Jackson <xavierejackson20@gmail.com>
2020-12-02 20:42:37 -0500

    made footer fixed and added z position of 100 and add place holder blocks

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,16 +6,28 @@ import XJ from "/Users/xavierjackson/Desktop/Homework/React Portfolio/reactportf
 function Home() {
   return (
     <div>
-      <div className="background">
-        <img src={XJ} alt="Background"></img>
+      <div>
+        <img className="background" src={XJ} alt="Background"></img>
       </div>
 
       <div className="container">
         <div className="row">
           <div className="col-4">
-            <h1>Home Page</h1>
-            <p>Home page information coming soon!</p>
+            <h1>Hello, my name is Xavier!</h1>
+            <p>
+              I am a Front-End Web Developer looking for opportunites to grow
+              and create{" "}
+            </p>
           </div>
+        </div>
+        <div className="row">
+          <div className="col test-shape"></div>
+        </div>
+        <div className="row">
+          <div className="col test-shape"></div>
+        </div>
+        <div className="row">
+          <div className="col test-shape"></div>
         </div>
         <div className="row">
           <div className="col test-shape"></div>
