8c0b4bff21c35e09dc1ef0b68d5b0907589d7891
Xavier Jackson <xavierejackson20@gmail.com>
2020-12-03 23:40:31 -0500

    moved text off of image on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,31 +6,35 @@ import XJ from "/Users/xavierjackson/Desktop/Homework/React Portfolio/reactportf
 function Home() {
   return (
     <div>
-      <div>
-        <img className="background" src={XJ} alt="Background"></img>
+      <div className="col-flex">
+        <img className="background" src={XJ} alt="Background" />
       </div>
-
+      {/* <hr className="section-divider"/> */}
       <div className="container">
         <div className="row">
-          <div className="col-4">
-            <h1>Hello, my name is Xavier!</h1>
+          <div className="col">
+            <h3>Hello, my name is Xavier!</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
             <p>
-              I am a Front-End Web Developer looking for opportunites to grow
-              and create{" "}
+              I am a Front-End Web Developer with Graphic Design experience
+              looking for entry-level opportunites in software development.
             </p>
           </div>
         </div>
         <div className="row">
-          <div className="col test-shape"></div>
+          <div className="col test-shape-0"></div>
         </div>
         <div className="row">
-          <div className="col test-shape"></div>
+          <div className="col test-shape-0"></div>
         </div>
         <div className="row">
-          <div className="col test-shape"></div>
+          <div className="col-flex test-shape-1"></div>
         </div>
         <div className="row">
-          <div className="col test-shape"></div>
+          <div className="col-flex test-shape-1"></div>
         </div>
       </div>
       <Footer />
