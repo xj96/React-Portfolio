4f98d30c567d7ee5483debd50937b5dc83644af0
Xavier Jackson <xavierejackson20@gmail.com>
2020-12-05 23:15:54 -0500

    updated text information on about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,18 +6,18 @@ function About() {
   return (
     <div>
       <div className="container">
-        <h1>Welcome!</h1>
+        <h3>About Me</h3>
         <p>
           I am an aspiring Front End Web Developer with graphic design
-          experience in logo creation and branding. Recently earned a
-          certificate from Columbia Engineering School in Full-Stack Development
-          to complement my skills as a graphic designer. Skills include HTML,
-          CSS, JavaScript, React.js, and responsive web design with mobile first
-          development. Strengths include being well organized, time efficient,
-          creative thinking, and working well in team-oriented environments that
-          are task driven. With each project, my aim is to create a UI that
-          users find easy to navigate and is appealing to the eye with current
-          trends in design. I applied these skills and strengths on a recent
+          experience focusing in logo creation and branding. Recently, I earned
+          a certificate from Columbia Engineering School in Software Development
+          to complement my skills as a graphic designer. My technical skills
+          include HTML, CSS, JavaScript, React.js, and responsive web design
+          with mobile first development. My strengths are being well organized,
+          time efficient, creative thinking, and working well in team-oriented
+          environments that are task driven. With each project, my aim is to
+          create a UI that users find easy to navigate and is appealing to the
+          eye with current trends in design. I applied these skills on a recent
           project that allows users to create an account, create tasks, write
           notes, organize a calendar, and manage their time more efficiently.
           I'm excited to leverage my skills as a web developer and a graphic
