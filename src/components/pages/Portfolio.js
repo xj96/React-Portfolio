a86a08767986a68cc7407216f74c50657ad827cb
Xavier Jackson <xavierejackson20@gmail.com>
2020-12-15 21:21:24 -0500

    added in link placeholders for portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Footer from "../Footer";
+import "/Users/xavierjackson/Desktop/Homework/React Portfolio/reactportfolio/src/components/Portfolio.css";
 import Placeholder from "/Users/xavierjackson/Desktop/Homework/React Portfolio/reactportfolio/src/images/orionthemes-placeholder-image.png";
 
 function Portfolio() {
   return (
     <div>
       <div className="container">
-        <h1>Portfolio Page</h1>
-        <p>Portfolio page content coming soon!</p>
-        <div className="card md-3" style={{maxWidth: "100%"}}>
+        <h1>Portfolio</h1>
+        <p>Here are my most recent projcets that I have been working on!</p>
+        <div className="card md-3" style={{ maxWidth: "100%" }}>
           <div className="row no-gutters">
             <div className="col-md-4">
               <img src={Placeholder} alt="placeholder"></img>
@@ -17,11 +18,18 @@ function Portfolio() {
               <div className="card-body">
                 <h5 className="card-title">Project Title</h5>
                 <p className="card-title">Project Description</p>
+                <a
+                  className="btn btn-primary"
+                  href="google.com"
+                  target="_blank"
+                >
+                  Click Here for Live Application!
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="card md-3" style={{maxWidth: "100%"}}>
+        <div className="card md-3" style={{ maxWidth: "100%" }}>
           <div className="row no-gutters">
             <div className="col-md-4">
               <img src={Placeholder} alt="placeholder"></img>
@@ -30,11 +38,18 @@ function Portfolio() {
               <div className="card-body">
                 <h5 className="card-title">Project Title</h5>
                 <p className="card-title">Project Description</p>
+                <a
+                  className="btn btn-primary"
+                  href="google.com"
+                  target="_blank"
+                >
+                  Click Here for Live Application!
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="card md-3" style={{maxWidth: "100%"}}>
+        <div className="card md-3" style={{ maxWidth: "100%" }}>
           <div className="row no-gutters">
             <div className="col-md-4">
               <img src={Placeholder} alt="placeholder"></img>
@@ -43,6 +58,13 @@ function Portfolio() {
               <div className="card-body">
                 <h5 className="card-title">Project Title</h5>
                 <p className="card-title">Project Description</p>
+                <a
+                  className="btn btn-primary"
+                  href="google.com"
+                  target="_blank"
+                >
+                  Click Here for Live Application!
+                </a>
               </div>
             </div>
           </div>
